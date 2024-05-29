0098943ab98466643ece623148b95cf47151c417
mcatelai <matthieu.catelain@free.fr>
2024-05-29 11:26:13 +0200

    modif DB

--- backend/pkg/db/migrations/sqlite/000003_create_posts_table.up.sql
+++ backend/pkg/db/migrations/sqlite/000003_create_posts_table.up.sql
@@ -92,3 +92,64 @@ CREATE TABLE IF NOT EXISTS liked_post (
 		FOREIGN KEY(post_id) REFERENCES posts(id),
 		FOREIGN KEY(user_id) REFERENCES users(id)
 	);
+
+
+CREATE TABLE IF NOT EXISTS POST_GROUP
+(
+    IDPost
+    INTEGER
+    PRIMARY
+    KEY AUTOINCREMENT,
+    UserID
+    INTEGER,
+    Title
+    VARCHAR
+(
+    150
+),
+    PostContent TEXT,
+    Date TEXT,
+    Image BLOB,
+    Private INTEGER, -- 0 Private, 1 Public, 2 Select user
+    Likes INTEGER, 
+    NbComments INTEGER,
+    FOREIGN KEY
+(
+    UserID
+) REFERENCES USERS
+(
+    ID
+)
+    );
+CREATE TABLE IF NOT EXISTS COMMENT_GROUP
+(
+    IDComment
+    INTEGER
+    PRIMARY
+    KEY AUTOINCREMENT,
+    IDPost
+    INTEGER,
+    UserID
+    INTEGER,
+    CommentContent
+    TEXT,
+    Date
+    TEXT,
+    Image
+    BLOB,
+    FOREIGN
+    KEY
+(
+    IDPost
+) REFERENCES POST
+(
+    IDPost
+),
+    FOREIGN KEY
+(
+    UserID
+) REFERENCES USERS
+(
+    ID
+)
+    );
