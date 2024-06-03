7438771eef1debf71d8e3c11ae1b0e7fb7a24737
folivier <fabien.olivier2@gmail.com>
2024-06-03 10:45:43 +0200

    :construction: [add] function WhoDisplayNotif

--- backend/pkg/notificationHandler.go
+++ backend/pkg/notificationHandler.go
@@ -156,3 +156,73 @@ func GetNotif(UserID int, db *sql.DB) (ListFollowers []User, listMP, listPost, l
 
 	return ListFollowers, listMP, listPost, listComment, listGroup, listEvent
 }
+
+// optionType = 'post', 'comment', 'group', 'event'
+// optionID = mettre l'id du comment ou du groupe ou de l'event
+func WhoDisplayNotif(UserID, optionID int, optionType string, db *sql.DB) (listUser []User) {
+
+	switch optionType {
+	case "post": // Uniquement si le post est public pour les autres option l'utilisateur choisie lors de la création du post
+		listUser = ListFollow(UserID, "followers", 0)
+		return listUser
+	case "comment":
+		// Récupére la privacité du post associé au commentaire 'optionID'
+		stmtPrivacitePost, err := db.Prepare(`SELECT POST.Private FROM POST
+												INNER JOIN COMMENT ON COMMENT.IDPost = POST.IDPost
+												WHERE COMMENT.IDComment = ?;`)
+		CheckErr(err, "WhoDisplayNotif stmtPrivacitePost comment, db prepare")
+		var private int
+		stmtPrivacitePost.QueryRow(optionID).Scan(&private)
+
+		// Si post public
+		if private == 1 { // Retourne la liste des followers de l'utilisateur
+			listUser = ListFollow(UserID, "followers", 0)
+		}
+		// Si post semi-privée
+		if private == 2 { // retourne la liste des utilisateurs correspondant dans la table 'POSTSELECTUSERS'
+			stmtComment, err := db.Prepare(`SELECT USERS.ID, USERS.FirstName, USERS.LastName, USERS.Avatar, USERS.Nickname FROM USERS
+												INNER JOIN POSTSELECTUSERS ON POSTSELECTUSERS.UserID = USERS.ID
+												WHERE POSTSELECTUSERS.IDPost = ?;`)
+			CheckErr(err, "WhoDisplayNotif comment, db prepare")
+			rowspost, err := stmtComment.Query(optionID)
+			CheckErr(err, "WhoDisplayNotif comment, db query")
+			for rowspost.Next() {
+				var currentUser User
+				err = rowspost.Scan(&currentUser.Id, &currentUser.Firstname, &currentUser.Lastname, &currentUser.Avatar, &currentUser.Nickname)
+				CheckErr(err, "WhoDisplayNotif comment, db rowspost.Next scan")
+				listUser = append(listUser, currentUser)
+			}
+		}
+		// Si privée, Ne rien faire et retourner listUser vide
+
+	case "group": // Retourne la liste des membres validé du groupe 'optionID'
+		stmtGroup, err := db.Prepare(`SELECT USERS.ID, USERS.FirstName, USERS.LastName, USERS.Avatar, USERS.Nickname FROM USERS
+										INNER JOIN MEMBERSGROUPS ON MEMBERSGROUPS.UserID = USERS.ID
+										WHERE MEMBERSGROUPS.IDgroup = ? AND MEMBERSGROUPS.ValidationInvite = true;`)
+		CheckErr(err, "WhoDisplayNotif group, db prepare")
+		rowspost, err := stmtGroup.Query(optionID)
+		CheckErr(err, "WhoDisplayNotif group, db query")
+		for rowspost.Next() {
+			var currentUser User
+			err = rowspost.Scan(&currentUser.Id, &currentUser.Firstname, &currentUser.Lastname, &currentUser.Avatar, &currentUser.Nickname)
+			CheckErr(err, "WhoDisplayNotif group, db rowspost.Next scan")
+			listUser = append(listUser, currentUser)
+		}
+
+	case "event": // Retourne la liste des membres qui ont validé l'event 'optionID'
+		stmtEvent, err := db.Prepare(`SELECT USERS.ID, USERS.FirstName, USERS.LastName, USERS.Avatar, USERS.Nickname FROM USERS
+										INNER JOIN RESPONSEEVENTGROUPS ON RESPONSEEVENTGROUPS.UserID = USERS.ID
+										WHERE RESPONSEEVENTGROUPS.IDEvent = ? AND RESPONSEEVENTGROUPS.Option = 1;`)
+		CheckErr(err, "WhoDisplayNotif event, db prepare")
+		rowspost, err := stmtEvent.Query(optionID)
+		CheckErr(err, "WhoDisplayNotif event, db query")
+		for rowspost.Next() {
+			var currentUser User
+			err = rowspost.Scan(&currentUser.Id, &currentUser.Firstname, &currentUser.Lastname, &currentUser.Avatar, &currentUser.Nickname)
+			CheckErr(err, "WhoDisplayNotif event, db rowspost.Next scan")
+			listUser = append(listUser, currentUser)
+		}
+	}
+
+	return listUser
+}
